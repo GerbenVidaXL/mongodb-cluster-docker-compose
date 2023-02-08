b5f05828230504d8f199ab86ec4af62cacfdb15e
GerbenVidaXL <107033851+GerbenVidaXL@users.noreply.github.com>
2023-02-08 11:32:00 +0100

    Update init-shard03.js

--- PSA/scripts/init-shard03.js
+++ PSA/scripts/init-shard03.js
@@ -3,8 +3,8 @@ rs.initiate(
       _id: "rs-shard-03",
       version: 1,
       members: [
-         { _id: 0, host : "shard03-a:27017" },
-         { _id: 1, host : "shard03-b:27017" }
+         { _id: 0, host : "mongo_shard03-a:27017" },
+         { _id: 1, host : "mongo_shard03-b:27017" }
       ]
    }
 )
