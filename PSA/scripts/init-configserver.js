4e1f9b6e89c45e7fcb6eac84a04170d3e1a68172
root <root@qas-cloud-docker-master-1.woger.local>
2023-02-08 12:40:07 +0000

    update script

--- PSA/scripts/init-configserver.js
+++ PSA/scripts/init-configserver.js
@@ -4,9 +4,9 @@ rs.initiate(
       configsvr: true,
       version: 1,
       members: [
-         { _id: 0, host : "mongo_configsvr01:27017" },
-         { _id: 1, host : "mongo_configsvr02:27017" },
-         { _id: 2, host : "mongo_configsvr03:27017" }
+         { _id: 0, host : "mongo2_configsvr01:27017" },
+         { _id: 1, host : "mongo2_configsvr02:27017" },
+         { _id: 2, host : "mongo2_configsvr03:27017" }
       ]
    }
 )
