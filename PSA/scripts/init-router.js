3dbe58fcf42085b5d9a0b010ce4d22965992a10e
GerbenVidaXL <107033851+GerbenVidaXL@users.noreply.github.com>
2023-02-08 11:31:08 +0100

    Update init-router.js

--- PSA/scripts/init-router.js
+++ PSA/scripts/init-router.js
@@ -1,8 +1,8 @@
-sh.addShard("rs-shard-01/shard01-a:27017")
-sh.addShard("rs-shard-01/shard01-b:27017")
+sh.addShard("rs-shard-01/mongo_shard01-a:27017")
+sh.addShard("rs-shard-01/mongo_shard01-b:27017")
 
-sh.addShard("rs-shard-02/shard02-a:27017")
-sh.addShard("rs-shard-02/shard02-b:27017")
+sh.addShard("rs-shard-02/mongo_shard02-a:27017")
+sh.addShard("rs-shard-02/mongo_shard02-b:27017")
 
-sh.addShard("rs-shard-03/shard03-a:27017")
-sh.addShard("rs-shard-03/shard03-b:27017")
+sh.addShard("rs-shard-03/mongo_shard03-a:27017")
+sh.addShard("rs-shard-03/mongo_shard03-b:27017")
